e88d60625f2b21b5defa8f92b72b344318cdf602
Jun Liang <illuminatemynight@gmail.com>
2019-06-22 13:38:33 +0000

    Automatically backed up by Learn

---
+++ lib/meal_choice.rb
@@ -0,0 +1,5 @@
+def meal_choice(veg1, veg2, protein = "meat")
+  puts "What a nutritious meal!"
+  puts "A place of #{protein} with #{veg1} and #{veg2}."
+  return "A place of #{protein} with #{veg1} and #{veg2}."
+end 
